acd7a7294ee1d8ddefe59f5cf6bddb1e6d5e1a9f
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2018-09-18 23:40:18 +0530

    Writing train.py

--- train.py
+++ train.py
@@ -18,3 +18,18 @@ from vgg import VGG
 #We need to define content and style Layers
 content_layers = ['re42']
 style_layers = ['re11','re21','re31','re41','re51']
+#setting seed for pytorch
+#torch.manual_seed(random.randint(1, 10000))
+#if you want to run the program on cuda then
+torch.cuda.manual_seed_all(random.randint(1, 10000))
+os.makedirs("images/")
+#The below flag allows you to enable the cudnn auto-tuner
+#to find the best algorithm for your hardware
+cudnn.benchmark = True
+
+#Dataset Processing
+transform = tf.Compose([
+    tf.Resize(512), #Default image_size
+    tf.ToTensor(),
+
+])
